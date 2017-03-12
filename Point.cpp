a4bb4ad0113c8abae57f4dbacc3bda34a1668a17
Jinwook Jung <jinwookjungs@gmail.com>
2017-03-12 16:48:20 -0400

    updated.

--- Point.cpp
+++ Point.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    Point.cpp
  * @author  Jinwook Jung (jinwookjungs@gmail.com)
- * @date    2017-03-06 15:19:31
+ * @date    2017-03-12 16:48:02
  *
  * Created on Mon Mar  6 15:19:31 2017.
  */
@@ -11,15 +11,12 @@
 Point::Point(void) : x(0), y(0) {
     //
 }
-
 Point::Point(double nx, double ny) : x(nx), y(ny) {
     //
 }
-
 Point::Point(const Point& src) : x(src.x), y(src.y) {
     //
 }
-
 Point::~Point(void){
     //
 }
@@ -28,42 +25,34 @@ Point& Point::operator=(const Point& src){
     x = src.x; y = src.y;
     return *this;
 }
-
 Point Point::operator*(const Point& alt){
     Point p(x*alt.x, y*alt.y);
     return p;
 }
-
 Point Point::operator/(const Point& alt){
     Point p(x/alt.x, y/alt.y);
     return p;
 }
-
 Point Point::operator-(const Point& alt){
     Point p(x-alt.x, y-alt.y);
     return p;
 }
-
 Point Point::operator+(const Point& alt){
     Point p(x+alt.x, y+alt.y);
     return p;
 }
-
 Point Point::operator*(double num){
     Point p(x*num, y*num);
     return p;
 }
-
 Point Point::operator/(double num){
     Point p(x/num, y/num);
     return p;
 }
-
 Point Point::operator-(double num){
     Point p(x-num, y-num);
     return p;
 }
-
 Point Point::operator+(double num){
     Point p(x+num, y+num);
     return p;
@@ -73,37 +62,30 @@ Point Point::operator*=(const Point& alt){
     x *= alt.x; y *= alt.y;
     return *this;
 }
-
 Point Point::operator/=(const Point& alt){
     x /= alt.x; y /= alt.y;
     return *this;
 }
-
 Point Point::operator-=(const Point& alt){
     x -= alt.x; y -= alt.y;
     return *this;
 }
-
 Point Point::operator+=(const Point& alt){
     x += alt.x; y += alt.y;
     return *this;
 }
-
 Point Point::operator*=(double num){
     x *= num; y *= num;
     return *this;
 }
-
 Point Point::operator/=(double num){
     x /= num; y /= num;
     return *this;
 }
-
 Point Point::operator-=(double num){
     x -= num; y -= num;
     return *this;
 }
-
 Point Point::operator+=(double num){
     x += num; y += num;
     return *this;
